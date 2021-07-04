b7363a248ee00cde0e5c836b8666ab876c2872de
Your Name <greenfitter516@gmail.com>
2021-07-04 21:02:44 +0600

    add

---
+++ class-5/bulidingAccess.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+$isWearingMusk =true;
+$isCovidNegative =true;
+$bodyTemparature =100;
+
+if($isWearingMusk && $isCovidNegative && $bodyTemparature <==98){
+
+	echo "Welcome to the building";
+
+}else {
+	echo "you cannot access the building";
+}
